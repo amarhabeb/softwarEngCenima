a791a70a9558be355699a0fc8fbbc9181d6ecf74
Julian Mour <82030307+julianmour@users.noreply.github.com>
2021-06-01 21:30:57 +0300

    renamed

---
+++ src/main/java/org/example/ChainManager.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ChainManager extends Manager{
+    public ChainManager(int ID, String name, String phoneNum, String email, String userName, String password) {
+        super(ID, name, phoneNum, email, userName, password);
+    }
+
+}
