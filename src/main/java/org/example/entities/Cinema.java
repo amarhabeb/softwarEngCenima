f5def3e677354e14a4fdda3c86305e304ea17060
Julian Mour <82030307+julianmour@users.noreply.github.com>
2021-06-28 11:00:55 +0300

    Update Cinema.java

--- src/main/java/org/example/entities/Cinema.java
+++ src/main/java/org/example/entities/Cinema.java
@@ -68,10 +68,10 @@ public class Cinema {
     }
     
     public String getBranch_name() {
-		return branch_name;
-	}
+	return branch_name;
+    }
 
-	public void setBranch_name(String branch_name) {
-		this.branch_name = branch_name;
-	}
+    public void setBranch_name(String branch_name) {
+	this.branch_name = branch_name;
+    }
 }
