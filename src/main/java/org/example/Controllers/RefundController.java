09b68eb4acdca44d9b365822701c231852a087ee
rayah <rayah.khatib.2@gmail.com>
2021-06-28 15:59:30 +0300

    Refund Controller edited

--- src/main/java/org/example/Controllers/RefundController.java
+++ src/main/java/org/example/Controllers/RefundController.java
@@ -5,7 +5,30 @@ import org.example.entities.Refund;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
 public class RefundController {
+
+    public static List<Refund> loadRefund(Session session) throws Exception{
+        try {
+            Transaction transaction = session.beginTransaction();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Refund> query = builder.createQuery(Refund.class);
+            query.from(Refund.class);
+            List<Refund> data = session.createQuery(query).getResultList();
+            transaction.commit();
+            return data;
+        } catch (Exception exception) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            System.err.println("An error occured, changes have been rolled back.");
+            exception.printStackTrace();
+            return null;
+        }
+    }
     public static boolean addRefund(Session session, Refund refund) throws Exception{
         try {
             Transaction transaction = session.beginTransaction();
@@ -22,4 +45,5 @@ public class RefundController {
             return false;
         }
     }
+
 }
