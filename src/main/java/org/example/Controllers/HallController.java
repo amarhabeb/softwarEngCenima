8e83bba3656f410ea836062ea25a5d33861583ef
AliAbuliel <aliabulielun@gmail.com>
2021-07-05 22:36:54 +0300

    add to hall controller calc maxSeats function

--- src/main/java/org/example/Controllers/HallController.java
+++ src/main/java/org/example/Controllers/HallController.java
@@ -1,6 +1,7 @@
 package org.example.Controllers;
 
 import org.example.entities.Hall;
+import org.example.entities.Regulations;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -11,13 +12,13 @@ import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class HallController {
-    public static List<Hall> loadHalls(Session session) throws Exception{
+    public static List<Hall> loadHalls(Session session) throws Exception {
         try {
             Transaction transaction = session.beginTransaction();
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<Hall> query = builder.createQuery(Hall.class);
-            Root<Hall> root=query.from(Hall.class);
-            query.where(builder.equal(root.get("active"),true));
+            Root<Hall> root = query.from(Hall.class);
+            query.where(builder.equal(root.get("active"), true));
             List<Hall> data = session.createQuery(query).getResultList();
             transaction.commit();
             return data;
@@ -31,7 +32,7 @@ public class HallController {
         }
     }
 
-    public static boolean addHall(Session session, Hall hall) throws Exception{
+    public static boolean addHall(Session session, Hall hall) throws Exception {
         try {
             Transaction transaction = session.beginTransaction();
             session.save(hall);
@@ -48,13 +49,13 @@ public class HallController {
         }
     }
 
-    public static boolean deleteHall(Session session, int hall_id){
+    public static boolean deleteHall(Session session, int hall_id) {
         try {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaUpdate<Hall> update_query = builder.createCriteriaUpdate(Hall.class);
-            Root<Hall> root=update_query.from(Hall.class);
+            Root<Hall> root = update_query.from(Hall.class);
             update_query.set("active", false);
-            update_query.where(builder.equal(root.get("ID"),hall_id));
+            update_query.where(builder.equal(root.get("ID"), hall_id));
             Transaction transaction = session.beginTransaction();
             session.createQuery(update_query).executeUpdate();
             transaction.commit();
@@ -70,4 +71,74 @@ public class HallController {
             return false;
         }
     }
+
+    public static Boolean calcMaxSeats(Session session) throws Exception {
+        try {
+            List<Regulations> reg = RegulationsController.loadReagulations(session);
+            Regulations reg1 = reg.get(0);
+
+            List<Hall> halls = HallController.loadHalls(session);
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaUpdate<Hall> update_query = builder.createCriteriaUpdate(Hall.class);
+            Root<Hall> root = update_query.from(Hall.class);
+            for (int i = 0; i < halls.size(); i++) {
+                Hall hall = halls.get(i);
+                if (reg1.getRegulations() == true) {
+                    int Y = reg1.getY();
+                    if (1.2 * Y < hall.getCapacity()) {
+                        update_query.set("maxSeats", Y);
+                        update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                        Transaction transaction = session.beginTransaction();
+                        session.createQuery(update_query).executeUpdate();
+                        transaction.commit();
+                        //session.clear();
+                        return true;
+                    } else {
+                        if (0.8 * Y < hall.getCapacity()) {
+                            update_query.set("maxSeats", (int) Math.round(0.8 * Y));
+                            update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                            Transaction transaction = session.beginTransaction();
+                            session.createQuery(update_query).executeUpdate();
+                            transaction.commit();
+                            //session.clear();
+                            return true;
+                        } else {
+
+                            update_query.set("maxSeats", (int) Math.round(0.5 * hall.getCapacity()));
+                            update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                            Transaction transaction = session.beginTransaction();
+                            session.createQuery(update_query).executeUpdate();
+                            transaction.commit();
+                            //session.clear();
+                            return true;
+                        }
+
+
+                    }
+
+                } else {
+                    update_query.set("maxSeats", hall.getCapacity());
+                    update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                    Transaction transaction = session.beginTransaction();
+                    session.createQuery(update_query).executeUpdate();
+                    transaction.commit();
+                    //session.clear();
+                    return true;
+                }
+
+            }
+
+
+        } catch (Exception exception) {
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            System.err.println("An error occurred, changes have been rolled back.");
+            exception.printStackTrace();
+            return false;
+        }
+        return false;
+
+    }
 }
+
