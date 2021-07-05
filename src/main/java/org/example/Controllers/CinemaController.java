999bf91d09560acbe44120e96b0362a3438e567c
AliAbuliel <aliabulielun@gmail.com>
2021-07-05 23:43:02 +0300

    move max seats function to cinema controller

--- src/main/java/org/example/Controllers/CinemaController.java
+++ src/main/java/org/example/Controllers/CinemaController.java
@@ -1,6 +1,8 @@
 package org.example.Controllers;
 
 import org.example.entities.Cinema;
+import org.example.entities.Hall;
+import org.example.entities.Regulations;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -8,6 +10,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.CriteriaUpdate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CinemaController {
@@ -70,4 +73,73 @@ public class CinemaController {
             return false;
         }
     }
+       public static Boolean calcMaxSeats(Session session, Cinema cinema) throws Exception {
+           try {
+               List<Regulations> reg = RegulationsController.loadReagulations(session);
+               Regulations reg1 = reg.get(0);
+
+               List<Hall> halls = cinema.getHalls();
+               CriteriaBuilder builder = session.getCriteriaBuilder();
+               CriteriaUpdate<Hall> update_query = builder.createCriteriaUpdate(Hall.class);
+               Root<Hall> root = update_query.from(Hall.class);
+               for (int i = 0; i < halls.size(); i++) {
+                   Hall hall = halls.get(i);
+                   if (reg1.getRegulations() == true) {
+                       int Y = reg1.getY();
+                       if (1.2 * Y < hall.getCapacity()) {
+                           update_query.set("maxSeats", Y);
+                           update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                           Transaction transaction = session.beginTransaction();
+                           session.createQuery(update_query).executeUpdate();
+                           transaction.commit();
+                           //session.clear();
+                           return true;
+                       } else {
+                           if (0.8 * Y < hall.getCapacity()) {
+                               update_query.set("maxSeats", (int) Math.round(0.8 * Y));
+                               update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                               Transaction transaction = session.beginTransaction();
+                               session.createQuery(update_query).executeUpdate();
+                               transaction.commit();
+                               //session.clear();
+                               return true;
+                           } else {
+
+                               update_query.set("maxSeats", (int) Math.round(0.5 * hall.getCapacity()));
+                               update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                               Transaction transaction = session.beginTransaction();
+                               session.createQuery(update_query).executeUpdate();
+                               transaction.commit();
+                               //session.clear();
+                               return true;
+                           }
+
+
+                       }
+
+                   } else {
+                       update_query.set("maxSeats", hall.getCapacity());
+                       update_query.where(builder.equal(root.get("ID"), hall.getID()));
+                       Transaction transaction = session.beginTransaction();
+                       session.createQuery(update_query).executeUpdate();
+                       transaction.commit();
+                       //session.clear();
+                       return true;
+                   }
+
+               }
+
+
+           } catch (Exception exception) {
+               if (session != null) {
+                   session.getTransaction().rollback();
+               }
+               System.err.println("An error occurred, changes have been rolled back.");
+               exception.printStackTrace();
+               return false;
+           }
+           return false;
+
+       }
+
 }
