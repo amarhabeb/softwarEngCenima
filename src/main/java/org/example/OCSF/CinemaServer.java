657c5f537b7d7b19886ea0e6b9548d6247128f46
rayah <rayah.khatib.2@gmail.com>
2021-08-01 01:55:20 +0300

    set Online movies On/Off added to MoviesController

--- src/main/java/org/example/OCSF/CinemaServer.java
+++ src/main/java/org/example/OCSF/CinemaServer.java
@@ -480,6 +480,7 @@ public class CinemaServer extends AbstractServer{
 					// adding tickit into  database
 					session.clear();
 					boolean success = TicketsController.addTicket(session, newticket);
+
 					//session.refresh(Ticket.class);
 					if (!success) {
 						throw new Exception("Ticket  couldnt be added");
