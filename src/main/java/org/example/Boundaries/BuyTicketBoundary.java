ebeead4ee314d365508c7b5e22bc7ea07a576e85
AliAbuliel <19062013*-Ali>
2021-08-01 13:23:37 +0300

    Edit Buy Ticket Boundary

--- src/main/java/org/example/Boundaries/BuyTicketBoundary.java
+++ src/main/java/org/example/Boundaries/BuyTicketBoundary.java
@@ -1,7 +1,9 @@
 package org.example.Boundaries;
 
+import javafx.beans.InvalidationListener;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,16 +14,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import org.example.App;
 import org.example.OCSF.CinemaClient;
+import org.example.entities.Cinema;
 import org.example.entities.Movie;
 import org.example.entities.Show;
 
@@ -39,6 +39,9 @@ import java.util.ResourceBundle;
 
         @FXML // fx:id="UpdateShowsTimesBtn"
         private Button ChooseShowBtn; // Value injected by FXMLLoader
+
+        @FXML
+        private ChoiceBox<String> cinemas;
 
 
 
@@ -107,8 +110,72 @@ import java.util.ResourceBundle;
 
 
 
+
         @Override
         public void initialize(URL url, ResourceBundle rb) {
+            ObservableList<Cinema> DataList1;
+
+            synchronized(CinemaClient.CinemasDataLock) {
+                org.example.Boundaries.Boundary.UpdateCinemasData();
+                // set items in table
+                 DataList1 = FXCollections.observableArrayList(CinemaClient.CinemasData);
+
+            }
+
+
+
+
+            for(int i =0;i<DataList1.size();i++){
+                cinemas.getItems().add(DataList1.get(i).getBranch_name());
+
+            }
+            // if the item of the list is changed
+            cinemas.getSelectionModel().selectedItemProperty().addListener((InvalidationListener) ov -> {
+                ObservableList<Cinema> DataList;
+                ObservableList<Show> DataList2;
+               List<Show> DataList3 = new LinkedList<>();
+                synchronized(CinemaClient.CinemasDataLock) {
+                    org.example.Boundaries.Boundary.UpdateCinemasData();
+                    // set items in table
+                     DataList = FXCollections.observableArrayList(CinemaClient.CinemasData);
+
+                }
+                Cinema c = null;
+                for(int i=0;i<DataList.size();i++){
+                    //System.out.println(cinemas.getSelectionModel().getSelectedItem().toString());
+                    if(DataList.get(i).getBranch_name().equals(cinemas.getSelectionModel().getSelectedItem().toString())){
+                         //System.out.println("Enter");
+                        c=DataList.get(i);
+                    }
+                }
+                synchronized(CinemaClient.ShowsDataLock) {
+                    org.example.Boundaries.Boundary.UpdateShowsData();
+                    // set items in table
+                   DataList2 = FXCollections.observableArrayList(CinemaClient.ShowsData);
+
+                    // System.out.println(DataList.get(0).getMovie().getName_en());
+                }
+                for(int i=0;i<DataList2.size();i++){
+                     //System.out.println(DataList2.get(i).getCinema().getID());
+                    if(DataList2.get(i).getCinema().getID()==c.getID()){
+                        DataList3.add(DataList2.get(i));
+
+
+                    }
+
+                }
+                ObservableList<Show> DataList5 = FXCollections.observableArrayList(DataList3);
+                ShowsTable.setDisable(false);
+               ShowsTable.setItems( DataList5);
+
+
+
+                // set the text for the label to the selected item
+                System.out.print("changed");
+            });
+
+
+
 
             // set-up the columns in the table
             movie_name.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Show, String>, ObservableValue<String>>() {
@@ -139,13 +206,13 @@ import java.util.ResourceBundle;
                 }
             });
 
-            synchronized(CinemaClient.ShowsDataLock) {
-                org.example.Boundaries.Boundary.UpdateShowsData();
-                // set items in table
-                ObservableList<Show> DataList = FXCollections.observableArrayList(CinemaClient.ShowsData);
-                ShowsTable.setItems(DataList);
-               // System.out.println(DataList.get(0).getMovie().getName_en());
-            }
+//            synchronized(CinemaClient.ShowsDataLock) {
+//                org.example.Boundaries.Boundary.UpdateShowsData();
+//                // set items in table
+//                ObservableList<Show> DataList = FXCollections.observableArrayList(CinemaClient.ShowsData);
+//                ShowsTable.setItems(DataList);
+//               // System.out.println(DataList.get(0).getMovie().getName_en());
+//            }
             System.out.println("initializing done");
         }
     }
