cd90f37803aed60a57c07bd4b7d95d462eacd15b
Julian Mour <82030307+julianmour@users.noreply.github.com>
2021-08-05 00:21:18 +0300

    log in done

--- src/main/java/org/example/Boundaries/EmployeeMainBoundary.java
+++ src/main/java/org/example/Boundaries/EmployeeMainBoundary.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.example.App;
+import org.example.OCSF.CinemaClient;
+import org.example.OCSF.CinemaClientCLI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,11 +16,36 @@ public abstract class EmployeeMainBoundary extends EmployeeBoundary{
 	
 	@FXML
     private Button backBtn;
+	
+	public static Boolean loggedOut = false;
+	synchronized void logOut() {
+		loggedOut = false;	// show isn't added yet
+		// create message and send it to the server
+    	LinkedList<Object> message = new LinkedList<Object>();
+		message.add("LogOut");
+		message.add(employee.getID());
+		synchronized(CinemaClient.EmployeeDataLock)
+		{	
+			CinemaClientCLI.sendMessage(message);
+							
+			// wait for Data to be changed
+			while(!loggedOut) {
+				try {
+					CinemaClient.EmployeeDataLock.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}	
+		}
+		// update ShowData
+		org.example.Boundaries.Boundary.UpdateEmployeesData();
+	}
 
     @FXML
     void clickBackBtn(ActionEvent event) throws IOException {
+    	logOut();
     	MessageBoundary.displayInfo("You logged out from your account. You are now in customer mode.");
-    	
     	App.setRoot("CustomerMain", null, stage);
     }
 	
